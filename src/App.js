4b57b2d79364fcbc89a39ee44cb3825c8c4ec4e1
naulesh123 <nkumar.nk1221@gmail.com>
2024-05-16 19:23:15 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+  hello
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function Homepage() {
+  return (
+    <div>
+
+      
+
+
+
+
+
+    </div>
+  )
+}
