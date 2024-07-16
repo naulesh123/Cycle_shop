6412402bc52326780c60ff59d0faac4a45c8d76e
Nikhil Kumar <93186639+naulesh123@users.noreply.github.com>
2024-07-17 01:34:54 +0530

    Update Corousel_scroller.jsx

--- frontend/src/Components/Corousel_scroller.jsx
+++ frontend/src/Components/Corousel_scroller.jsx
@@ -9,7 +9,7 @@ const arr=[];
 
 for(let i=0;i<props.product_photo.length;i++)
   {
-  const imgurl=`https://cycle-shop-hpxr4-dxnl3qv7q-naulesh123s-projects.vercel.app/uploads/${props.product_photo[i]}`;
+  const imgurl=`https://cycle-shop5.vercel.app//uploads/${props.product_photo[i]}`;
   arr.push(<Corousel_scroller_element products_url={imgurl}/>)
   
   
